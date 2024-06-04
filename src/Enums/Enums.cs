fa4a91a1eb2b3c0279701719c776508801b43f4b
Mayar.Abuzahra <mayar.abuzahra@we-universe.com>
2024-06-04 11:42:33 +0300

    Upload first five solutions

---
+++ src/Enums/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace codecrafters_http_server.src.Enums
+{
+    enum HTTPStatusCodesEnum
+    {
+        Ok = 200,
+        NotFound = 404
+    }
+
+    enum DataSize
+    {
+        Kilobyte = 1024
+    }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,73 @@
+using codecrafters_http_server.src.Enums;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+namespace codecrafters_http_server
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Program Logs will appear here!");
 
-// Uncomment this block to pass the first stage
-// TcpListener server = new TcpListener(IPAddress.Any, 4221);
-// server.Start();
-// server.AcceptSocket(); // wait for client
+            TcpListener server = new TcpListener(IPAddress.Any, 4221);
+            server.Start();
+
+            try
+            {
+                string httpResponse = "HTTP/1.1 ";
+                byte[] data = new byte[(int)DataSize.Kilobyte];
+                string okMessage = $"{httpResponse}{(int)HTTPStatusCodesEnum.Ok} OK\r\n";
+                string notFoundMessage = $"{httpResponse}{(int)HTTPStatusCodesEnum.NotFound} Not Found\r\n\r\n";
+
+                while (true)
+                {
+                    Socket socket = server.AcceptSocket();
+
+                    int dataSize = socket.Receive(data);
+                    string requestData = Encoding.UTF8.GetString(data, 0, dataSize);
+
+                    var requestLines = requestData.Split("\r\n");
+                    var requestParts = requestLines[0].Split("/");
+
+                    if (requestParts.Length > 1 && requestParts[1] == " HTTP")
+                        httpResponse = okMessage;
+                    else if (requestParts[1].StartsWith("echo"))
+                        httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {requestParts[2].Trim().Split(' ')[0].Length}\r\n\r\n{requestParts[2].Trim().Split(' ')[0]}";
+                    else if (requestParts[1].StartsWith("user-agent"))
+                        httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {requestLines[2].Trim().Split(' ')[1].Length} \r\n\r\n{requestLines[2].Trim().Split(' ')[1]}";
+                    else if (requestParts[1].StartsWith("files"))
+                    {
+                        // ./server.sh --directory <directory>
+                        string[] commandLineArgs = Environment.GetCommandLineArgs();
+
+                        // <directory> is the third argument
+                        string dir = commandLineArgs[2];
+                        string fileName = requestParts[1].Split('/')[1];
+                        string filePath = $"{dir}{fileName}";
+
+                        if (File.Exists(filePath))
+                        {
+                            string fileContent = File.ReadAllText(filePath);
+                            httpResponse = $"{httpResponse}{(int)HTTPStatusCodesEnum.Ok} OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {fileContent.Length}\r\n\r\n{fileContent}";
+                        }
+                        else
+                            httpResponse = notFoundMessage;
+                    }
+                    else
+                    {
+                        httpResponse = notFoundMessage;
+                    }
+
+                    socket.Send(Encoding.UTF8.GetBytes(httpResponse));
+                    socket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.StackTrace);
+            }
+        }
+    }
+}
