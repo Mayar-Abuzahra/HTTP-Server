b263da79a0520ede5002b1c176338f261d9a281b
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2024-06-14 18:47:49 +0300

    Update Server.cs

--- src/Server.cs
+++ src/Server.cs
@@ -5,107 +5,106 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace codecrafters_http_server
+namespace codecrafters_http_server;
+
+public class Server
 {
-    public class Server
+    public static void Main()
     {
-        public static void Main()
+        Console.WriteLine("Program Logs will appear here!");
+
+        try
         {
-            Console.WriteLine("Program Logs will appear here!");
+            TcpListener server = new TcpListener(IPAddress.Any, 4221);
+            server.Start();
 
-            try
+            string httpResponse = string.Empty;
+            byte[] data = new byte[(int)DataSizeEnum.Kilobyte];
+            string okMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.Ok} OK\r\n";
+            string notFoundMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.NotFound} Not Found\r\n\r\n";
+            string createdMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.Created} Created\r\n\r\n";
+
+            while (true)
             {
-                TcpListener server = new TcpListener(IPAddress.Any, 4221);
-                server.Start();
+                Socket socket = server.AcceptSocket();
 
-                string httpResponse = string.Empty;
-                byte[] data = new byte[(int)DataSizeEnum.Kilobyte];
-                string okMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.Ok} OK\r\n";
-                string notFoundMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.NotFound} Not Found\r\n\r\n";
-                string createdMessage = $"HTTP/1.1 {(int)HTTPStatusCodesEnum.Created} Created\r\n\r\n";
+                int dataSize = socket.Receive(data);
+                string requestData = Encoding.UTF8.GetString(data, 0, dataSize);
 
-                while (true)
+                var requestLines = requestData.Split("\r\n");
+                var requestParts = Helper.SplitString(requestLines[0], '/');
+
+                if (requestParts.Length > 1 && requestParts[1] == " HTTP")
+                    httpResponse = $"{okMessage}\r\n";
+                else if (requestParts[1].StartsWith("echo"))
                 {
-                    Socket socket = server.AcceptSocket();
+                    string[] encodingHeader = (!string.IsNullOrEmpty(requestLines[2])) ? Helper.SplitString(Helper.SplitString(requestLines[2], ':')[1].Trim(), ',') : [];
+                    bool containsGzip = encodingHeader.Any(header => string.Equals(header.Trim(), "gzip", StringComparison.OrdinalIgnoreCase));
 
-                    int dataSize = socket.Receive(data);
-                    string requestData = Encoding.UTF8.GetString(data, 0, dataSize);
-
-                    var requestLines = requestData.Split("\r\n");
-                    var requestParts = Helper.SplitString(requestLines[0], '/');
-
-                    if (requestParts.Length > 1 && requestParts[1] == " HTTP")
-                        httpResponse = $"{okMessage}\r\n";
-                    else if (requestParts[1].StartsWith("echo"))
+                    if (containsGzip)
                     {
-                        string[] encodingHeader = (!string.IsNullOrEmpty(requestLines[2])) ? Helper.SplitString(Helper.SplitString(requestLines[2], ':')[1].Trim(), ',') : [];
-                        bool containsGzip = encodingHeader.Any(header => string.Equals(header.Trim(), "gzip", StringComparison.OrdinalIgnoreCase));
+                        byte[] uncompressedData = Encoding.UTF8.GetBytes(Helper.SplitString(requestParts[2], ' ')[0]);
 
-                        if (containsGzip)
-                        {
-                            byte[] uncompressedData = Encoding.UTF8.GetBytes(Helper.SplitString(requestParts[2], ' ')[0]);
-
-                            using MemoryStream compressedStream = new MemoryStream();
-                            using GZipStream gzipStream = new GZipStream(compressedStream, CompressionMode.Compress, true);
+                        using MemoryStream compressedStream = new MemoryStream();
+                        using GZipStream gzipStream = new GZipStream(compressedStream, CompressionMode.Compress, true);
                             
-                            gzipStream.Write(uncompressedData, 0, uncompressedData.Length);
-                            gzipStream.Flush();
-                            gzipStream.Close();
+                        gzipStream.Write(uncompressedData, 0, uncompressedData.Length);
+                        gzipStream.Flush();
+                        gzipStream.Close();
 
-                            byte[] compressedData = compressedStream.ToArray();
-                            httpResponse = $"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {compressedData.Length}\r\n\r\n";
+                        byte[] compressedData = compressedStream.ToArray();
+                        httpResponse = $"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {compressedData.Length}\r\n\r\n";
 
-                            socket.Send([.. Encoding.UTF8.GetBytes(httpResponse), .. compressedData]);
-                            socket.Close();
+                        socket.Send([.. Encoding.UTF8.GetBytes(httpResponse), .. compressedData]);
+                        socket.Close();
 
-                            break;
-                        }
-                        else
-                            httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {Helper.SplitString(requestParts[2], ' ')[0].Length}\r\n\r\n{Helper.SplitString(requestParts[2], ' ')[0]}";
+                        break;
                     }
-                    else if (requestParts[1].StartsWith("user-agent"))
-                        httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {Helper.SplitString(requestLines[2], ' ')[1].Length} \r\n\r\n{Helper.SplitString(requestLines[2], ' ')[1]}\r\n";
-                    else if (requestParts[1].StartsWith("files"))
+                    else
+                        httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {Helper.SplitString(requestParts[2], ' ')[0].Length}\r\n\r\n{Helper.SplitString(requestParts[2], ' ')[0]}";
+                    }
+                else if (requestParts[1].StartsWith("user-agent"))
+                    httpResponse = $"{okMessage}Content-Type: text/plain\r\nContent-Length: {Helper.SplitString(requestLines[2], ' ')[1].Length} \r\n\r\n{Helper.SplitString(requestLines[2], ' ')[1]}\r\n";
+                else if (requestParts[1].StartsWith("files"))
+                {
+                    // ./server.sh --directory <directory>
+                    string[] commandLineArgs = Environment.GetCommandLineArgs();
+
+                    string dir = commandLineArgs[2];
+                    string fileName = Helper.SplitString(requestParts[2], ' ')[0];
+                    string filePath = $"{dir}{fileName}";
+
+                    if (requestParts[0].StartsWith("GET"))
                     {
-                        // ./server.sh --directory <directory>
-                        string[] commandLineArgs = Environment.GetCommandLineArgs();
-
-                        string dir = commandLineArgs[2];
-                        string fileName = Helper.SplitString(requestParts[2], ' ')[0];
-                        string filePath = $"{dir}{fileName}";
-
-                        if (requestParts[0].StartsWith("GET"))
+                        if (File.Exists(filePath))
                         {
-                            if (File.Exists(filePath))
-                            {
-                                string fileContent = File.ReadAllText(filePath);
-                                httpResponse = $"{okMessage}Content-Type: application/octet-stream\r\nContent-Length: {fileContent.Length}\r\n\r\n{fileContent}";
-                            }
-                            else
-                                httpResponse = notFoundMessage;
-                        }
-                        else if (requestParts[0].StartsWith("POST"))
-                        {
-                            string fileContent = requestLines[requestLines.Length - 1];
-                            File.WriteAllText(filePath, fileContent);
-                            httpResponse = createdMessage;
+                            string fileContent = File.ReadAllText(filePath);
+                            httpResponse = $"{okMessage}Content-Type: application/octet-stream\r\nContent-Length: {fileContent.Length}\r\n\r\n{fileContent}";
                         }
                         else
                             httpResponse = notFoundMessage;
-                    }
-                    else
+                        }
+                    else if (requestParts[0].StartsWith("POST"))
                     {
-                        httpResponse = notFoundMessage;
+                        string fileContent = requestLines[requestLines.Length - 1];
+                        File.WriteAllText(filePath, fileContent);
+                        httpResponse = createdMessage;
                     }
-
-                    socket.Send(Encoding.UTF8.GetBytes(httpResponse));
-                    socket.Close();
+                else
+                    httpResponse = notFoundMessage;
                 }
+                else
+                {
+                    httpResponse = notFoundMessage;
+                }
+
+                socket.Send(Encoding.UTF8.GetBytes(httpResponse));
+                socket.Close();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: " + e.StackTrace);
-            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.StackTrace);
         }
     }
 }
